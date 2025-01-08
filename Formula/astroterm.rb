2395d3856ca32bbc26108ab878a4c6f749367ae4
Dalton Luce <daltonluce42@gmail.com>
2025-01-08 18:28:59 -0500

    Fix formula and move to v1.0.2

--- Formula/astroterm.rb
+++ Formula/astroterm.rb
@@ -1,21 +1,21 @@
 class Astroterm < Formula
-  desc "Terminal-based celestial viewer written in C using ncurses"
+  desc "Celestial viewer for the terminal, written in C with ncurses"
   homepage "https://github.com/da-luce/astroterm/"
-  url "https://github.com/da-luce/astroterm/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e2a9500ebb53189397701e8e9e051807b564642b2d1670e75366222678ca361c"
+  url "https://github.com/da-luce/astroterm/archive/refs/tags/v1.0.2.tar.gz"
+  version "1.0.1"
+  sha256 "48441dcce970f97dc8c554dfd671c3f268d9517592446ef4df0cdd934603edaa"
   license "MIT"
 
   depends_on "argtable" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-
-  depends_on "argtable"
-  depends_on "ncurses"
+  depends_on "pkg-config" => :build
   uses_from_macos "ncurses" => :build
 
   def install
     system "curl", "-L", "-o", "data/bsc5", "http://tdc-www.harvard.edu/catalogs/BSC5"
-    system "meson", "setup", "build", "-Drelease_build=true", *std_meson_args
+    # On linuxbrew, the static argtable2.a lib is NOT PIE, so we must force to compile our executable as not a pie
+    system "meson", "setup", "build", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
   end
