27c73f39d290e327737ff45f9755c4099e248a8e
Dalton Luce <102048662+da-luce@users.noreply.github.com>
2025-01-09 03:51:55 +0000

    astroterm: add 1.0.2 bottle.

--- Formula/astroterm.rb
+++ Formula/astroterm.rb
@@ -5,6 +5,13 @@ class Astroterm < Formula
   sha256 "48441dcce970f97dc8c554dfd671c3f268d9517592446ef4df0cdd934603edaa"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/da-luce/homebrew-astroterm/releases/download/astroterm-1.0.2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b10ca5638c1d91177d9586c08909c1752e72eafb6dfe79f3ca03b7c56f30e900"
+    sha256 cellar: :any_skip_relocation, ventura:       "15beacca90bd3d92bb85b63a31ccc3ee4e7eefc538e56b65a7b648b6bcd2c6d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec68a8fc9ffbbc979aff768d010ba3137b473851ba294654254052edf8dffd53"
+  end
+
   depends_on "argtable" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
